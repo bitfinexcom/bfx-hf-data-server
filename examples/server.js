9b5cd50a0019673f45fe6d2b5b7e534b4cd5340e
Cris Mihalache <me@f3rno.com>
2018-10-05 15:03:22 +0300

    (refactor) simplify example script, remove vestigial elements

--- examples/server.js
+++ examples/server.js
@@ -6,25 +6,17 @@ require('bfx-hf-util/lib/catch_uncaught_errors')
 
 const debug = require('debug')('hf:data-server:examples:server')
 const bfx = require('./bfx')
-const { onExit: OnExit } = require('bfx-hf-util')
 
 const logDataDBInfo = require('../lib/db/log_info')
 const auditDB = require('../lib/db/audit')
 const startDataServer = require('../lib/start')
 
 const rest = bfx.rest(2)
-const ws = bfx.ws(2)
-
-OnExit(() => (ws && ws.isOpen()) && ws.close(), -2)
-
-ws.on('error', debug)
 
 const run = async () => {
   await logDataDBInfo()
 
   const ds = startDataServer({
-    candles: true,
-    trades: true,
     port: 8899,
     rest
   })
