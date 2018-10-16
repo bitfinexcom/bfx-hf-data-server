e6df730f8da35b03fffe33ecdb952c24877d9f83
Cris Mihalache <me@f3rno.com>
2018-10-16 16:14:49 +0300

    (refactor) splice in new models

--- examples/server.js
+++ examples/server.js
@@ -1,28 +1,27 @@
 'use strict'
 
-process.env.DEBUG = 'hf:*'
+process.env.DEBUG = 'bfx:hf:*'
 
 require('bfx-hf-util/lib/catch_uncaught_errors')
 
-const debug = require('debug')('hf:data-server:examples:server')
+const debug = require('debug')('bfx:hf:data-server:examples:server')
 const bfx = require('./bfx')
+const { connectDB, startDB } = require('bfx-hf-models')
 
 const logDataDBInfo = require('../lib/db/log_info')
-const auditDB = require('../lib/db/audit')
 const startDataServer = require('../lib/start')
 
-const rest = bfx.rest(2)
+const rest = bfx.rest(2, { transform: true })
 
 const run = async () => {
+  await startDB(`${__dirname}/../db`)
+  await connectDB('hf-data-server')
   await logDataDBInfo()
 
   const ds = startDataServer({
     port: 8899,
     rest
   })
-
-  await auditDB(ds)
-  await logDataDBInfo()
 }
 
 try {
