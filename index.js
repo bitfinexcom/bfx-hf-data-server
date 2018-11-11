429919566595b882ce497d7dfbee25f40a88ff78
Cris Mihalache <me@f3rno.com>
2018-11-11 15:52:15 +0100

    (refactor) fix proxy auth, export server, support agent

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 'use strict'
 
-const startDataServer = require('./lib/start')
-const logDataDBInfo = require('./lib/db/log_info')
-
-module.exports = {
-  startDataServer,
-  logDataDBInfo
-}
+module.exports = require('./lib/server')
