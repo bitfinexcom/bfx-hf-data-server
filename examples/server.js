6513894c4426112cfe0961db50d78bcc40d5c00a
Cris Mihalache <me@f3rno.com>
2018-11-11 10:51:46 +0100

    (refactor) create DataServer class, refactor to implement

--- examples/server.js
+++ examples/server.js
@@ -5,23 +5,17 @@ process.env.DEBUG = 'bfx:hf:*'
 require('bfx-hf-util/lib/catch_uncaught_errors')
 
 const debug = require('debug')('bfx:hf:data-server:examples:server')
-const bfx = require('./bfx')
 const { connectDB, startDB } = require('bfx-hf-models')
 
 const logDataDBInfo = require('../lib/db/log_info')
-const startDataServer = require('../lib/start')
-
-const rest = bfx.rest(2, { transform: true })
+const DataServer = require('../lib/server')
 
 const run = async () => {
   await startDB(`${__dirname}/../db`)
   await connectDB('hf-data-server')
   await logDataDBInfo()
 
-  const ds = startDataServer({
-    port: 8899,
-    rest
-  })
+  const ds = new DataServer({ port: 8899 })
 }
 
 try {
--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 
 const startDataServer = require('./lib/start')
 const logDataDBInfo = require('./lib/db/log_info')
-const auditDB = require('./lib/db/audit')
 
 module.exports = {
   startDataServer,
-  logDataDBInfo,
-  auditDB
+  logDataDBInfo
 }
