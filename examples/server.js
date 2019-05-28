4031cd96081859b9482a3c8df3c7e663b21f91ee
Cris Mihalache <me@f3rno.com>
2019-05-28 12:41:46 +0700

    (WIP)

--- examples/server.js
+++ examples/server.js
@@ -4,22 +4,5 @@ process.env.DEBUG = 'bfx:hf:*'
 
 require('bfx-hf-util/lib/catch_uncaught_errors')
 
-const debug = require('debug')('bfx:hf:data-server:examples:server')
-const { connectDB, startDB } = require('bfx-hf-models')
-
-const logDataDBInfo = require('../lib/db/log_info')
 const DataServer = require('../lib/server')
-
-const run = async () => {
-  await startDB(`${__dirname}/../db`)
-  await connectDB('hf-data-server')
-  await logDataDBInfo()
-
-  const ds = new DataServer({ port: 8899 })
-}
-
-try {
-  run()
-} catch (e) {
-  debug(e.stack)
-}
+new DataServer({ port: 8899 })
