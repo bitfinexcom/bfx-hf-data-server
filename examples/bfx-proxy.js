39004146acd044d9efa816a8d7061efe1fc976a2
Cris Mihalache <me@f3rno.com>
2019-05-29 23:47:04 +0700

    (refactor/feature) add ERRORS map/std sendError(), support new models [untested]

--- examples/bfx-proxy.js
+++ examples/bfx-proxy.js
@@ -4,23 +4,10 @@ process.env.DEBUG = 'bfx:hf:*'
 
 require('bfx-hf-util/lib/catch_uncaught_errors')
 
-const debug = require('debug')('bfx:hf:data-server:examples:server')
-const { connectDB, startDB } = require('bfx-hf-models')
 const DataServer = require('../lib/server')
 
-const run = async () => {
-  await startDB(`${__dirname}/../db`)
-  await connectDB('hf-data-server')
-
-  const ds = new DataServer({
-    proxy: true,
-    transform: true,
-    port: 8899
-  })
-}
-
-try {
-  run()
-} catch (e) {
-  debug(e.stack)
-}
+new DataServer({
+  proxy: true,
+  transform: true,
+  port: 8899
+})
--- examples/server.js
+++ examples/server.js
@@ -5,4 +5,5 @@ process.env.DEBUG = 'bfx:hf:*'
 require('bfx-hf-util/lib/catch_uncaught_errors')
 
 const DataServer = require('../lib/server')
+
 new DataServer({ port: 8899 })
