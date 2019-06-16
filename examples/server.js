e0822aa9bce0301273afd5cafef6c478d06857d4
Cris Mihalache <me@f3rno.com>
2019-06-16 21:18:59 +0700

    (refactor) preliminary changes for new bfx-hf-models API

--- examples/server.js
+++ examples/server.js
@@ -5,6 +5,22 @@ process.env.DEBUG = 'bfx:hf:*'
 require('bfx-hf-util/lib/catch_uncaught_errors')
 require('dotenv').config()
 
+const HFDB = require('bfx-hf-models')
+const HFDBLowDBAdapter = require('bfx-hf-models-adapter-lowdb')
+const { schema: HFDBBitfinexSchema } = require('bfx-hf-ext-plugin-bitfinex')
+
 const DataServer = require('../lib/server')
 
-new DataServer({ port: 8899 })
+const { DB_FILENAME } = process.env
+const db = new HFDB({
+  schema: HFDBBitfinexSchema,
+  adapter: HFDBLowDBAdapter({
+    dbPath: `${__dirname}/../${DB_FILENAME}`,
+    schema: HFDBBitfinexSchema
+  })
+})
+
+new DataServer({
+  port: 8899,
+  db,
+})
