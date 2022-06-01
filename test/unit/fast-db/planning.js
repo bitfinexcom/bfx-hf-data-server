61d79f37bfe79ff999f3c200e5faae4b78d6cd2f
Tarcisio Ferraz <tarcisio.ferraz@bitfinex.com>
2022-06-01 09:49:41 -0300

    rename intervar vars

--- test/unit/fast-db/planning.js
+++ test/unit/fast-db/planning.js
@@ -8,25 +8,27 @@ const { fillTheGaps } = require('../../../lib/fast-db/planning')
 describe('Planning execution', () => {
   describe('#fillTheGaps', function () {
     const framesInDisk = [
-      { low: 3, high: 7 },
-      { low: 20, high: 25 },
-      { low: 10, high: 20 },
-      { low: 32, high: 40 },
-      { low: 8, high: 21 },
-      { low: 68, high: 75 }
+      { start: 3, end: 7 },
+      { start: 20, end: 25 },
+      { start: 10, end: 20 },
+      { start: 32, end: 40 },
+      { start: 8, end: 21 },
+      { start: 68, end: 75 }
     ]
 
     it('should fill the gaps that needs to be loaded', () => {
-      const result = fillTheGaps(framesInDisk, 1, 50)
+      const start = 1
+      const end = 50
+      const result = fillTheGaps(framesInDisk, start, end)
 
       expect(result).to.be.eql([
-        { low: 1, high: 2, pending: true },
-        { low: 3, high: 7, offset: 3, limit: 7 },
-        { low: 8, high: 21, offset: 8, limit: 21 },
-        { low: 20, high: 25, offset: 22, limit: 25 },
-        { low: 26, high: 31, pending: true },
-        { low: 32, high: 40, offset: 32, limit: 40 },
-        { low: 41, high: 50, pending: true }
+        { start: 1, end: 2, pending: true },
+        { start: 3, end: 7, offset: 3, limit: 7 },
+        { start: 8, end: 21, offset: 8, limit: 21 },
+        { start: 20, end: 25, offset: 22, limit: 25 },
+        { start: 26, end: 31, pending: true },
+        { start: 32, end: 40, offset: 32, limit: 40 },
+        { start: 41, end: 50, pending: true }
       ])
     })
   })
