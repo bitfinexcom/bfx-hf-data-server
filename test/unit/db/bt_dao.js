fa920a4cddc8f82e2b0f360e6243a9d07f9387c2
Hemant <tekwani.hemant@gmail.com>
2023-08-12 20:02:35 +0530

    Unit tests added for Sqlite DB and Backtest DAO functions.

---
+++ test/unit/db/bt_dao.js
@@ -0,0 +1,90 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+'use strict'
+
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+const { expect } = require('chai')
+let btDao
+const testBt = {
+  exchange: 'bitfinex',
+  strategyId: '123456',
+  start: 1691491641045,
+  end: 1691577141045,
+  symbol: 'TESTBTC:TESTUSD',
+  timeframe: '5m',
+  includeCandles: true,
+  includeTrades: false,
+  candleSeed: 10,
+  sync: true,
+  meta: 'd80adf68-f672-4f78-a111-5d72585a2c43',
+  capitalAllocation: 13,
+  stopLossPerc: 56,
+  maxDrawdownPerc: 76,
+  isFavorite: 0,
+  timestamp: Date.now()
+}
+
+describe('Backtest DAO', () => {
+  const executeQueryStub = sinon.stub()
+  const createDataStub = sinon.stub()
+  const getDataStub = sinon.stub()
+  const queryDataStub = sinon.stub()
+
+  before(() => {
+    getDataStub.returns(testBt)
+    queryDataStub.returns([testBt])
+
+    btDao = proxyquire('../../../lib/db/bt_dao',
+      {
+        './sqlite_db': {
+          executeQuery: executeQueryStub,
+          createData: createDataStub,
+          getData: getDataStub,
+          queryData: queryDataStub
+        }
+      })
+  })
+
+  describe('#saveBt', () => {
+    it('save backtest', async () => {
+      const args = [
+        'bitfinex', '123456', 1691491641045, 1691577141045, 'TESTBTC:TESTUSD',
+        '5m', true, false, 10, true, {}, 'd80adf68-f672-4f78-a111-5d72585a2c43',
+        { capitalAllocation: 13, stopLossPerc: 56, maxDrawdownPerc: 76 }
+      ]
+      await btDao.saveBt(args)
+      expect(createDataStub.calledOnce).to.be.true
+    })
+  })
+
+  describe('#getBtHistory', () => {
+    it('get backtest history for a strategy', async () => {
+      const strategyId = '123456'
+      const data = await btDao.getBtHistory(strategyId)
+      expect(data).to.be.an('array')
+      expect(data.length).to.be.greaterThan(0)
+      expect(queryDataStub.calledOnce).to.be.true
+    })
+  })
+
+  describe('#getBtData', () => {
+    it('gets backtest data by id', async () => {
+      const strategyId = '123456'
+      const id = 1
+      const data = await btDao.getBtData(id)
+      expect(data).to.be.an('object')
+      expect(data.strategyId).to.be.equal(strategyId)
+      expect(getDataStub.calledOnce).to.be.true
+    })
+  })
+
+  describe('#setBtFavorite', () => {
+    it('sets backtest as favorite', async () => {
+      const id = 1
+      const isFavorite = 1
+      await btDao.setBtFavorite(id, isFavorite)
+      expect(executeQueryStub.calledOnce).to.be.true
+    })
+  })
+})
