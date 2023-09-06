64e0a2ae7345855aa8986d3839baed4a5c295294
tekwani <tekwani.hemant@gmail.com>
2023-09-06 23:29:22 +0530

    Save backtest history in sqlite. (#60)
    * Save backtest history in sqlite.
    Get backtest history by strategyId.
    Get backtest history strategy details by id.
    Set backtest strategy as favorite.
    
    * Sqlite Db connection error check added.
    symbol column datatype size increase.
    
    * Typo fix.
    
    * Unit tests added for Sqlite DB and Backtest DAO functions.
    
    * Save bt result to db. Replaced meta with executionId. Added function to delete bt.
    
    * BtData: parse btResult
    
    * send bt id as second argument in responce array
    
    * Added endpoint to delete all bts by strategy ID.
    
    * send bt full data on single request
    
    * normalize bt history response data
    
    * send message on success bt saving
    
    * Return error if backtest execution fails.
    Changed saveBt callback to async call.
    Fixed unit test.
    
    * typo fix
    
    * Call sqlite connection method from server open method
    
    * log sqlite connection error
    
    ---------
    
    Co-authored-by: Dmytro Shcherbonos <dmytro.sh@code-care.pro>

---
+++ test/unit/db/bt_dao.js
@@ -0,0 +1,81 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+'use strict'
+
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+const { expect } = require('chai')
+let btDao
+const testBt = {
+  exchange: 'bitfinex',
+  strategyId: '123456',
+  start: 1691491641045,
+  end: 1691577141045,
+  symbol: 'TESTBTC:TESTUSD',
+  timeframe: '5m',
+  includeCandles: true,
+  includeTrades: false,
+  candleSeed: 10,
+  sync: true,
+  executionId: 'd80adf68-f672-4f78-a111-5d72585a2c43',
+  capitalAllocation: 13,
+  stopLossPerc: 56,
+  maxDrawdownPerc: 76,
+  isFavorite: 0,
+  timestamp: Date.now()
+}
+
+describe('Backtest DAO', () => {
+  const executeQueryStub = sinon.stub()
+  const createDataStub = sinon.stub()
+  const getDataStub = sinon.stub()
+  const queryDataStub = sinon.stub()
+
+  before(() => {
+    getDataStub.returns(testBt)
+    queryDataStub.returns([testBt])
+
+    btDao = proxyquire('../../../lib/db/bt_dao',
+      {
+        './sqlite_db': {
+          executeQuery: executeQueryStub,
+          createData: createDataStub,
+          getData: getDataStub,
+          queryData: queryDataStub
+        }
+      })
+  })
+
+  describe('#saveBt', () => {
+    it('save backtest', async () => {
+      const args = [
+        'bitfinex', '123456', 1691491641045, 1691577141045, 'TESTBTC:TESTUSD',
+        '5m', true, false, 10, true, {}, 'd80adf68-f672-4f78-a111-5d72585a2c43',
+        { capitalAllocation: 13, stopLossPerc: 56, maxDrawdownPerc: 76 }
+      ]
+      const savedBt = await btDao.saveBt(args, {})
+      expect(savedBt).to.be.an('object')
+      expect(savedBt.strategyId).to.be.equal('123456')
+      expect(createDataStub.calledOnce).to.be.true
+    })
+  })
+
+  describe('#getBtHistory', () => {
+    it('get backtest history for a strategy', async () => {
+      const strategyId = '123456'
+      const data = await btDao.getBtHistory(strategyId)
+      expect(data).to.be.an('array')
+      expect(data.length).to.be.greaterThan(0)
+      expect(queryDataStub.calledOnce).to.be.true
+    })
+  })
+
+  describe('#setBtFavorite', () => {
+    it('sets backtest as favorite', async () => {
+      const executionId = testBt.executionId
+      const isFavorite = 1
+      await btDao.setBtFavorite(executionId, isFavorite)
+      expect(executeQueryStub.calledOnce).to.be.true
+    })
+  })
+})
---
+++ test/unit/db/sqlite_db.js
@@ -0,0 +1,72 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+'use strict'
+
+const { expect } = require('chai')
+const os = require('os')
+const fs = require('fs')
+const sqliteDb = require('../../../lib/db/sqlite_db')
+
+const testSqlitePath = `${os.homedir()}/.bitfinexhoney/testsqlite`
+const testBtValues = ['bitfinex', '123456', 1691491641045, 1691577141045, 'TESTBTC:TESTUSDT', '5m',
+  true, false, 10, true, 'd80adf68-f672-4f78-a111-5d72585a2c43', 13, 56, 76, 0, Date.now()]
+
+describe('Sqlite DB', () => {
+  before(async () => {
+    if (!fs.existsSync(testSqlitePath)) {
+      fs.mkdirSync(testSqlitePath)
+    }
+    await sqliteDb.connectDb(testSqlitePath)
+  })
+
+  after(() => {
+    if (fs.existsSync(testSqlitePath)) {
+      fs.rmSync(testSqlitePath, { recursive: true, force: true })
+    }
+  })
+
+  describe('#createData', () => {
+    it('adds new entry in bt_history table', async () => {
+      const createQuery = 'insert into bt_history (exchange, strategyId, start, end, symbol, timeframe, includeCandles, includeTrades, candleSeed, sync, executionId, capitalAllocation, stopLossPerc, maxDrawdownPerc, isFavorite, timestamp) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
+      await sqliteDb.createData(createQuery, testBtValues)
+      const getQuery = 'SELECT * FROM bt_history'
+      const data = await sqliteDb.queryData(getQuery)
+      expect(data).to.be.an('array')
+      expect(data.length).to.be.greaterThan(0)
+    })
+  })
+
+  describe('#getData', () => {
+    it('gets data by id from bt_history table', async () => {
+      const strategyId = '123456'
+      const getQuery = 'SELECT * FROM bt_history where strategyId=?'
+      const data = await sqliteDb.getData(getQuery, [strategyId])
+      expect(data).to.be.an('object')
+      expect(data.strategyId).to.be.equal(strategyId)
+    })
+  })
+
+  describe('#queryData', () => {
+    it('gets data rows by query from bt_history table', async () => {
+      const getQuery = 'SELECT * FROM bt_history'
+      const data = await sqliteDb.queryData(getQuery)
+      expect(data).to.be.an('array')
+      expect(data.length).to.be.greaterThan(0)
+    })
+  })
+
+  describe('#executeQuery', () => {
+    it('executes query to update data in bt_history table', async () => {
+      const id = 1
+      const isFavorite = 1
+      const query = 'update bt_history set isFavorite=? where id=?'
+      const values = [isFavorite, id]
+      await sqliteDb.executeQuery(query, values)
+
+      const getQuery = 'SELECT * FROM bt_history where id=?'
+      const data = await sqliteDb.getData(getQuery, [id])
+      expect(data.id).to.be.equal(id)
+      expect(data.isFavorite).to.be.equal(isFavorite)
+    })
+  })
+})
